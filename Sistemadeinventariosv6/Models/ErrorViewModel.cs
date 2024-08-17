d277967c994ef388fcdf5e36b6c6589146427046
adal <adalmendez02@gmail.com>
2024-08-17 13:52:22 -0600

    proyecto

---
+++ Sistemadeinventariosv6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistemadeinventariosv6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
